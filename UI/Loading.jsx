cc16c7892d6075fea2e42f616858904090ab27dc
SoheilAsgari <soheilasgari183@gmail.com>
2024-11-30 20:37:47 +0330

    fetch-data-List

---
+++ UI/Loading.jsx
@@ -0,0 +1,16 @@
+
+import { ThreeDots } from 'react-loader-spinner'
+
+function Loading() {
+  return (
+  <>
+   <ThreeDots
+  height="75"
+  width="70"
+  radius="9"
+  color="red"/>
+  </>
+  )
+}
+
+export default Loading
